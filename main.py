16dfe576df95894bfe6817a33230f105f7f22972
xNyxDev <xnyxdev@gmail.com>
2025-05-05 18:42:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ import time
 import requests as request
 from deep_translator import GoogleTranslator
 
-# Obtiene las claves de la API de las variables de entorno
 API_KEY = os.environ.get("API_KEY")
 API_SECRET = os.environ.get("API_SECRET")
 BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
 API_TOKEN = os.environ.get("API_TOKEN")
 API_TOKEN_SECRET = os.environ.get("API_TOKEN_SECRET")
 
-# Verifica si todas las variables de entorno requeridas están configuradas
 if not all([API_KEY, API_SECRET, BEARER_TOKEN, API_TOKEN, API_TOKEN_SECRET]):
     print("Error: Faltan una o más variables de entorno requeridas.")
-    exit(1)  # Usa sys.exit(1) para una salida más limpia con código de error
+    exit(1)
 
 client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, API_TOKEN, API_TOKEN_SECRET)
 
@@ -22,22 +20,33 @@ def obtener_datos_interesantes():
     try:
         res = request.get("https://uselessfacts.jsph.pl/random.json?language=es")
         if res.status_code == 200:
-            traducido = GoogleTranslator(source='auto', target='es').translate(res.json().get("text"))
-            return traducido
+            data = res.json()
+            dato_en = data.get("text")
+            if dato_en:
+                traducido = GoogleTranslator(source='en', target='es').translate(dato_en)
+                return traducido
+            else:
+                print("Error: La respuesta JSON no contiene el campo 'text'.")
+                return None
         else:
-            print("Error al obtener datos interesantes")
+            print(f"Error al obtener datos interesantes. Código de estado: {res.status_code}")
+            return None
     except Exception as e:
         print(f"Error: {e}")
+        return None
 
 while True:
     dato = obtener_datos_interesantes()
-    try:
-        client.create_tweet(text="Dato del Dia: " + dato)
-        print("Tweet enviado con éxito")
-    except Exception as e:
-        print(f"Error al enviar el tweet: {e}")
+    if dato:
+        try:
+            client.create_tweet(text="Dato del Dia: " + dato)
+            print("Tweet enviado con éxito")
+        except Exception as e:
+            print(f"Error al enviar el tweet: {e}")
+    else:
+        print("No se pudo obtener un dato interesante. No se enviará ningún tweet.")
 
-    time.sleep(120)  # Espera 1 hora antes de enviar el siguiente tweet
+    time.sleep(120)
 
 if __name__ == "__main__":
-    main()
+    pass
